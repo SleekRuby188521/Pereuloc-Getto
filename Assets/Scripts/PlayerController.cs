18fad382f1c356fd9244006179047e2f679a2eb2
SleekRuby188521 <asleekruby@gmail.com>
2024-04-13 22:38:18 +0500

    PlayerMovement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform player;
+    
+    public float speed = 0;
+
+    private CharacterController _characterController;
+    private Vector3 _moveVector;
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MovementUpdate();
+    }
+
+    private void MovementUpdate()
+    {
+        _moveVector = Vector3.zero;
+        
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+            ;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+            
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+            
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+            
+        }
+    }
+
+    void FixedUpdate()
+    {
+        _characterController.Move(_moveVector * speed * Time.deltaTime);
+    }
+
+}
