6ea097264412ace813066990e92d3d91e7727374
NEZzzerak <159697264+NEZzzerak@users.noreply.github.com>
2024-04-27 14:07:55 +0300

    FlashLight
    Da budet Svet!!!

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    [SerializeField] Light _flashLight;
+    [SerializeField] AudioSource _xzKakNazzvat;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            _flashLight.enabled = !_flashLight.enabled;
+            _xzKakNazzvat.Play();
+        }
+    }
+}
