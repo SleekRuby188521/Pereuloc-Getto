35abf1253ca2d37f2432c90e0e9b0a9d783cf22f
NEZzzerak <159697264+NEZzzerak@users.noreply.github.com>
2024-04-27 17:15:33 +0300

    BB
    11

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -13,7 +13,7 @@ public class CameraRotation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -28,5 +28,6 @@ public class CameraRotation : MonoBehaviour
 
         newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
         CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
+       
     }
 }
---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 10f;
+    public Camera PlayerCamera;
+    public AudioSource wee;
+
+    private float nextTimeToFire=0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && Time.time>=nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+    public void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(PlayerCamera.transform.position,PlayerCamera.transform.forward,out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            wee.Play();
+        }
+    }
+}
