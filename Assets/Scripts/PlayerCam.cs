d479ebbd7bbdd82489372ed1416cde2cee9840dc
NEZzzerak <159697264+NEZzzerak@users.noreply.github.com>
2024-04-29 17:21:15 +0300

    BB
    я устал

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform Orientation;
+
+    float xRotation;
+    float yRotation;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float MouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += MouseX;
+        xRotation -= MouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+
+        Orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+
+    }
+}
