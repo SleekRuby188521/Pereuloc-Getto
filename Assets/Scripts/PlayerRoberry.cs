88223d05a71df3ebcc2f85d60b5d497ddf65d7b3
SleekRuby188521 <asleekruby@gmail.com>
2024-05-01 19:25:38 +0500

    Roberry Update

---
+++ Assets/Scripts/PlayerRoberry.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRoberry : MonoBehaviour
+{
+    public AudioSource audiosource;
+    public AudioClip moneysound;
+
+    public GameObject target;
+    void Update()
+    {
+        if(Vector3.Distance(target.transform.position, transform.position) <= 3f && Input.GetKeyUp(KeyCode.E))
+        {
+            audiosource.PlayOneShot(moneysound);
+        }
+    }
+}
