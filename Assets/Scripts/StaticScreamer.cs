916f380d7ee5a91f03611f4c377e0e8483d474cd
SleekRuby188521 <asleekruby@gmail.com>
2024-05-08 13:30:38 +0500

    StaticScreamerUpdate 08.05.24

---
+++ Assets/Scripts/StaticScreamer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticScreamer : MonoBehaviour
+{
+    public bool isActive = true;
+
+    public GameObject monster;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (isActive)
+            {
+                isActive = false;
+                monster.SetActive(true);
+                StartCoroutine(Timer());
+            }
+        }
+    }
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(1);
+        monster.SetActive(false);
+    }
+}
