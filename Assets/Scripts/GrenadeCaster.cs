4235275af9c6af04627e2f1ab6227ec8567e10de
SleekRuby188521 <asleekruby@gmail.com>
2024-05-12 17:48:26 +0500

    GrenadeUpdateOne

---
+++ Assets/Scripts/GrenadeCaster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeCaster : MonoBehaviour
+{
+    public float grenadeCount = 5;
+
+    public Rigidbody grenadePrefab;
+    public Transform grenadeSourceTransform;
+
+    public float force = 10;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (grenadeCount > 0)
+            {
+                var grenade = Instantiate(grenadePrefab);
+                grenade.transform.position = grenadeSourceTransform.position;
+                grenade.GetComponent<Rigidbody>().AddForce(grenadeSourceTransform.forward * force);
+                grenadeCount--;
+            }
+        }   
+    }
+}
