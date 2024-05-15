1b7a79c1fdfa671af31cbfe93f3733fd0a182df7
SleekRuby188521 <asleekruby@gmail.com>
2024-05-15 14:24:03 +0500

    CameraMarketUpdate

---
+++ Assets/Scripts/MarketTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MarketTrigger : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera[] virtualCameras;
+    private int currenCameraIndex;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        virtualCameras[currenCameraIndex].Priority = 0;
+        currenCameraIndex++;
+
+        if (currenCameraIndex >= virtualCameras.Length)
+            currenCameraIndex = 0;
+
+        virtualCameras[currenCameraIndex].Priority = 1;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        virtualCameras[currenCameraIndex].Priority = 0;
+        currenCameraIndex--;
+        virtualCameras[currenCameraIndex].Priority = 1;
+    }
+}
